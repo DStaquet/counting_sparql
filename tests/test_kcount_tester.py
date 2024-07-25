8ef6a2489ba538de7165de5c77cd486ec82b0794
Dore <dore.staquet@uhasselt.be>
2024-07-25 16:17:11 +0200

    Pytests for k_counts on BGP

--- tests/test_kcount_tester.py
+++ tests/test_kcount_tester.py
@@ -14,15 +14,29 @@ def test_build_data():
 
     kcount_tester.build_data(
         "data/dataset100.ttl",
-        "data/delete_product98.ttl",
         "output",
         "Queries/berlin_benchmark/100/query3_benchmark/query3_99.sparql",
         duckdb_conn,
+        "data/delete_product98.ttl",
+        "data/insert_product105.ttl",
     )
     g_data = duckdb_conn.sql("SELECT * FROM G;").df()
     delta_g_data = duckdb_conn.sql(
-        "SELECT * FROM delta_G;"
+        "SELECT * FROM delta_G where s = 'http://www4.wiwiss.fu-berlin.de/bizer/bsbm/v01/instances/dataFromProducer3/Product98';"
     ).df()
-    nu_g_data = duckdb_conn.sql("SELECT * FROM nu_G;").df()
+    assert not delta_g_data.empty
+    delta_g_data = duckdb_conn.sql(
+        "SELECT * FROM delta_G where s = 'http://www4.wiwiss.fu-berlin.de/bizer/bsbm/v01/instances/dataFromProducer3/Product105';"
+    ).df()
+    assert not delta_g_data.empty
+
+    nu_g_data = duckdb_conn.sql(
+        "SELECT * FROM nu_G WHERE s = 'http://www4.wiwiss.fu-berlin.de/bizer/bsbm/v01/instances/dataFromProducer3/Product98';"
+    ).df()
+    assert nu_g_data.empty
+    nu_g_data = duckdb_conn.sql(
+        "SELECT * FROM nu_G WHERE s = 'http://www4.wiwiss.fu-berlin.de/bizer/bsbm/v01/instances/dataFromProducer3/Product105';"
+    ).df()
+    assert not nu_g_data.empty
 
     assert g_data.shape[0] != None
