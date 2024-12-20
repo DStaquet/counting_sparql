35f0a0d2d1b3585964927b842fde7a0f80eb7eb1
Dore <dore.staquet@uhasselt.be>
2024-12-20 02:21:36 +0100

    Updated the benchmark for bsbm

--- README.md
+++ README.md
@@ -8,13 +8,21 @@ Prerequisites
     - DuckDB
     - pandas
 
+For the Tri hop experiment
+--------------------------
+- The user can run the following command for the tri-hop queries experiment:
+    ```
+    $ ./benchmark.sh
+    ```
+- In case this does not work, the commands in said script can be run manually.
+
+For the BSBM experiment
+-----------------------
 - Set up the data
     - This script uses the data generated using the Berlin SPARQL Benchmark (BSBM) dataset. The source code to generate the data can be downloaded from the following link: https://sourceforge.net/projects/bsbmtools/
     1) The data is generated using the following commands:
         ```bash
-        $ ./generate -pc 100 -s ttl -fn dataset100
-        $ ./generate -pc 1000 -s ttl -fn dataset1000
-        $ ./generate -pc 5000 -s ttl -fn dataset5000
+        $ ./generate -pc 1000 -ud -tc 1000 -fn chain_1000 -ufn chain_update_1000 -s ttl  
         ```
     2) Put these datasets in the `data/` directory
     3) After this run the following command to generate the queries and delta updates
--- benchmark_bsbm.sh
+++ benchmark_bsbm.sh
@@ -1,12 +1,16 @@
 
 echo 'Constructing 1000 products for the BSBM queries'
-python3 example_constructor/bsbm_setup.py example_constructor/bsbm_data/input_data/new_dataset.ttl example_constructor/bsbm_data/input_data/test_1000.nt example_constructor/bsbm_data/output_data/1000_product
-
-
+python3 example_constructor/bsbm_setup.py bsbm_data/input_data/chain_1000.ttl bsbm_data/input_data/short_1000.nt bsbm_data/output_data/1000_product
 
 
 echo 'Running the benchmark for Query1 with 1000 products'
-python3 main.py benchmarker/Queries/BSBM/Query1/query1.sparql temp_tests/chain_benchmark -d example_constructor/bsbm_data/output_data/1000_product/base.csv -dl example_constructor/bsbm_data/output_data/1000_product -nf example_constructor/bsbm_data/output_data/1000_product -m -r 5 -s 
+python3 main.py benchmarker/Queries/BSBM/Query1/query1.sparql temp_tests/chain_benchmark -d bsbm_data/output_data/1000_product/base.csv -dl bsbm_data/output_data/1000_product -nf bsbm_data/output_data/1000_product -m -r 5 -s -icsv -ocsv measurements/output_bsbm.csv > measurements/bsbm_query1.txt
 
 echo 'Running the benchmark for Query2 with 1000 products'
-python3 main.py benchmarker/Queries/BSBM/Query2/query2.sparql temp_tests/chain_benchmark_temp -d bsbm_data/output_data/1000_product/base.csv -dl bsbm_data/output_data/1000_product -nf bsbm_data/output_data/1000_product -m -r 5 -s
+python3 main.py benchmarker/Queries/BSBM/Query2/query2.sparql temp_tests/chain_benchmark_temp -d bsbm_data/output_data/1000_product/base.csv -dl bsbm_data/output_data/1000_product -nf bsbm_data/output_data/1000_product -m -r 5 -s -ocsv measurements/output_bsbm.csv > measurements/bsbm_query2.txt
+
+echo 'Running the benchmark for Query3 with 1000 products'
+python3 main.py benchmarker/Queries/BSBM/Query3/query3.sparql temp_tests/chain_benchmark_temp -d bsbm_data/output_data/1000_product/base.csv -dl bsbm_data/output_data/1000_product -nf bsbm_data/output_data/1000_product -m -r 5 -s -ocsv measurements/output_bsbm.csv > measurements/bsbm_query3.txt
+
+echo 'Running the benchmark for Query4 with 1000 products'
+python3 main.py benchmarker/Queries/BSBM/Query4/query4.sparql temp_tests/chain_benchmark_temp -d bsbm_data/output_data/1000_product/base.csv -dl bsbm_data/output_data/1000_product -nf bsbm_data/output_data/1000_product -m -r 5 -s -ocsv measurements/output_bsbm.csv > measurements/bsbm_query4.txt
