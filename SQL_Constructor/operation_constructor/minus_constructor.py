fd3ea7b2d2379c6f88bd8b29d0db0756882ee914
Dore <dore.staquet@uhasselt.be>
2025-08-05 12:20:51 +0200

    Cleaned up minus constructor file

--- SQL_Constructor/operation_constructor/minus_constructor.py
+++ SQL_Constructor/operation_constructor/minus_constructor.py
@@ -1,17 +1,13 @@
-from SQL_Constructor.table_constructor import (
-    __encode_table_name,
-)
+"""Modules to import"""
+
+from rdflib.plugins.sparql.parserutils import CompValue
+
 from SQL_Constructor.base_constructor import make_join
 from SQL_Constructor.base_constructor import make_group_by
 from SQL_Constructor.operation_constructor.diff_constructor import (
     diff_query_sub,
     delta_diff_sub,
 )
-
-
-from rdflib.plugins.sparql.parserutils import CompValue
-from rdflib.term import Variable
-
 from SQL_Constructor.table_constructor import (
     create_table_w_select,
 )
@@ -19,8 +15,8 @@ from SQL_Constructor.table_constructor import (
 
 def minus_query(
     part: CompValue,
-    schemas1: list[set[str]] = [],
-    schemas2: list[set[str]] = [],
+    schemas1: list[set[str]] | None = None,
+    schemas2: list[set[str]] | None = None,
 ) -> str:
     """Generates the minus query.
 
@@ -30,18 +26,23 @@ def minus_query(
     Returns:
         str: Query string for the minus operation.
     """
+    if schemas1 is None:
+        schemas1 = []
+    if schemas2 is None:
+        schemas2 = []
+
     diff_dict: dict[str, list[str]] = diff_query_sub(
         part, (schemas1, schemas2), minus=True
     )
 
     all_minus_queries: str = ""
-    for key in diff_dict:
-        if len(diff_dict[key]) != 1:
+    for key, item in diff_dict.items():
+        if len(item) != 1:
             raise ValueError(
                 "Minus query should only have one query per schema."
             )
         all_minus_queries += create_table_w_select(
-            key, diff_dict[key][0]
+            key, item[0]
         )
 
     return all_minus_queries
