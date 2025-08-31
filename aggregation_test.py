9b4ab0e281e48e89a79bf30bb9f0a91ce52c5bfd
Dore <dore.staquet@uhasselt.be>
2025-08-31 17:56:57 +0200

    First test on aggregation

---
+++ aggregation_test.py
@@ -0,0 +1,195 @@
+"""Tests a quick aggregation query with SUM in the projection."""
+
+import argparse
+import os
+import sys
+
+from time import time
+
+from duckdb import connect, DuckDBPyConnection
+
+from experiments.time_operators import timing_per_operator
+from build_data import (
+    load_delta_table_in_graph,
+    load_table_in_graph,
+    setup_query_files,
+    readQueryFile,
+    get_query_object,
+)
+from setup_queries import get_query_output_dir
+
+
+def prep_aggregation_query(
+    duckdb_conn: DuckDBPyConnection,
+    args_namespace: argparse.Namespace,
+) -> None:
+    """Prepares and runs an aggregation query."""
+
+    # Setup query files
+    setup_query_files(
+        args_namespace.query_file, args_namespace.query_dir
+    )
+
+    q_query_object = get_query_object(
+        readQueryFile(args_namespace.query_file)
+    )
+    # algebra.pprintAlgebra(q_query_object)
+
+    # Output directory
+    query_output_dir: str = get_query_output_dir(
+        args_namespace.query_dir, q_query_object
+    )
+
+    load_table_in_graph(
+        args_namespace.data_file,
+        duckdb_conn,
+    )
+    load_delta_table_in_graph(
+        args_namespace.delta_file,
+        duckdb_conn,
+        args_namespace.nu_file,
+    )
+
+    duckdb_conn.execute(
+        readQueryFile(
+            os.path.join(
+                query_output_dir, "drop_tables.sql"
+            )
+        )
+    )
+    duckdb_conn.execute(
+        readQueryFile(
+            os.path.join(
+                query_output_dir,
+                "drop_delta_tables.sql",
+            )
+        )
+    )
+
+    timing_per_operator(
+        q_query_object.algebra.p,
+        query_output_dir,
+        duckdb_conn,
+    )
+
+    timing_per_operator(
+        q_query_object.algebra.p,
+        query_output_dir,
+        duckdb_conn,
+        increm=True,
+    )
+
+
+if __name__ == "__main__":
+    hashseed = os.getenv("PYTHONHASHSEED")
+    if not hashseed:
+        os.environ["PYTHONHASHSEED"] = "0"
+        os.execv(
+            sys.executable, [sys.executable] + sys.argv
+        )
+
+    parser = argparse.ArgumentParser(
+        description="Time aggregation query."
+    )
+    parser.add_argument(
+        "query_file",
+        type=str,
+        help="The query file to run.",
+    )
+    parser.add_argument(
+        "query_dir",
+        type=str,
+        help="The directory where the SQL queries are stored.",
+    )
+    parser.add_argument(
+        "data_file",
+        type=str,
+        help="The base graph data file to use.",
+    )
+    parser.add_argument(
+        "delta_file",
+        type=str,
+        help="The delta graph data file to use.",
+    )
+    parser.add_argument(
+        "nu_file",
+        type=str,
+        help="The nu file to use.",
+    )
+    parser.add_argument(
+        "--runs",
+        type=int,
+        default=3,
+        help="The number of runs to average over.",
+    )
+    parser.add_argument(
+        "--db",
+        type=str,
+        default=":memory:",
+        help="The database to connect to.",
+    )
+    parser.add_argument(
+        "aggregation_sql_file",
+        type=str,
+        help="The SQL file that contains the aggregation operator.",
+    )
+    parser.add_argument(
+        "aggregation_delta_sql_file",
+        type=str,
+        help="The SQL file that contains the aggregation operator for the delta.",
+    )
+    parser.add_argument(
+        "aggregation_nu_sql_file",
+        type=str,
+        help="The SQL file that contains the aggregation operator for the nu.",
+    )
+
+    args = parser.parse_args()
+
+    conn: DuckDBPyConnection = connect(
+        database=args.db, read_only=False
+    )
+
+    with open(
+        args.aggregation_sql_file, "r", encoding="utf-8"
+    ) as f:
+        aggregation_sql = f.read()
+    with open(
+        args.aggregation_delta_sql_file,
+        "r",
+        encoding="utf-8",
+    ) as f:
+        aggregation_delta_sql = f.read()
+    with open(
+        args.aggregation_nu_sql_file,
+        "r",
+        encoding="utf-8",
+    ) as f:
+        aggregation_nu_sql = f.read()
+
+    prep_aggregation_query(
+        conn,
+        args,
+    )
+
+    print("Running aggregation incrementally...")
+    conn.execute("DROP TABLE IF EXISTS Agg;")
+    conn.execute("DROP TABLE IF EXISTS nu_Agg_increm;")
+    conn.execute(aggregation_sql)
+    conn.execute("DROP TABLE IF EXISTS delta_Agg;")
+    start_time = time()
+    conn.execute(aggregation_delta_sql)
+    end_time = time()
+    print(
+        f"Delta aggregation completed in {(end_time - start_time)*1000} milliseconds."
+    )
+
+    conn.execute("DROP TABLE IF EXISTS nu_Agg;")
+    start_time = time()
+    conn.execute(aggregation_nu_sql)
+    end_time = time()
+    print(
+        f"Scratch aggregation completed in {(end_time - start_time)*1000} milliseconds."
+    )
+
+    print("Done.")
---
+++ benchmarker/Queries/BSBM/Query1/selfmade_files/delta_agg.sql
@@ -0,0 +1,37 @@
+CREATE TABLE delta_Agg AS
+SELECT Agg.product,
+       (CAST (delta_Agg.value1 AS INT) + CAST (Agg.value1 AS INT)) AS value1,
+       1 AS k_count
+FROM delta_Project_6206501852706074180 AS delta_Agg, Project_6206501852706074180 AS Agg
+WHERE (delta_Agg.product = Agg.product)
+UNION
+SELECT delta_Agg.product,
+       CAST (delta_Agg.value1 AS INT) AS value1,
+       1 AS k_count
+FROM delta_Project_6206501852706074180 AS delta_Agg
+WHERE delta_Agg.product NOT IN (SELECT product FROM Project_6206501852706074180)
+UNION
+SELECT Agg.product,
+       Agg.value1 AS value1,
+       -Agg.k_count AS k_count
+FROM Project_6206501852706074180 AS Agg, delta_Project_6206501852706074180 AS delta_Agg
+WHERE delta_Agg.product = Agg.product;
+
+CREATE TABLE nu_Agg_increm AS
+SELECT (CASE
+            WHEN R1.product NOT NULL THEN R1.product
+            ELSE R2.product
+        END) AS product,
+         (CASE
+            WHEN R1.value1 NOT NULL THEN CAST (R1.value1 AS INT)
+            ELSE CAST (R2.value1 AS INT)
+        END) AS value1,
+         (CASE
+            WHEN R1.k_count IS NULL THEN R2.k_count
+            WHEN R2.k_count IS NULL THEN R1.k_count
+            ELSE R1.k_count + R2.k_count
+        END) AS k_count
+FROM Agg AS R1
+FULL OUTER JOIN delta_Agg AS R2 ON R1.product = R2.product
+AND R1.value1 = R2.value1
+WHERE coalesce(R1.k_count, 0) + coalesce(R2.k_count, 0) > 0;
