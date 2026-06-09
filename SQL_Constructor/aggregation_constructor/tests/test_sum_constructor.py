bc29c85b5c949d86d896b544e0e5c567ff5c3126
Dore <dore.staquet@uhasselt.be>
2026-06-09 15:17:11 +0200

    Updated output unit test aggregation sum

--- SQL_Constructor/aggregation_constructor/tests/test_sum_constructor.py
+++ SQL_Constructor/aggregation_constructor/tests/test_sum_constructor.py
@@ -3,12 +3,16 @@
 from pytest import mark
 
 from rdflib.plugins.sparql.parserutils import CompValue
-from rdflib.term import Variable
+from duckdb import DuckDBPyConnection, connect
+
+# from rdflib.term import Variable
 
 from SQL_Constructor.aggregation_constructor.sum_constructor import (
     sum_join_query,
     delta_sum_join_query,
 )
+
+from SQL_Constructor.table_constructor import get_table_name
 
 from SQL_Constructor.operation_constructor.tests.base_functions import (
     reset_seed,
@@ -31,7 +35,7 @@ AGGREGATION_SAMPLE = CompValue("Aggregate_Sample", vars="y")
         ),
     ],
 )
-def test_sum_join_query_output(
+def test_sum_join_query(
     query_file: str,
     aggregate_values: list[CompValue],
     aggregate_sample: CompValue,
@@ -58,3 +62,70 @@ def test_sum_join_query_output(
         print("Expected Output:\n", expected_output)
 
         assert output in expected_output
+
+
+def _build_bgps(bgp_name: str, duckdb_conn: DuckDBPyConnection) -> None:
+    """Helper function to build the BGP tables for testing."""
+    duckdb_conn.execute(f"""
+        CREATE OR REPLACE TABLE {bgp_name} AS
+        SELECT 1 AS x, 'Product1' AS y, 1 as k_count
+        UNION ALL
+        SELECT 3 AS x, 'Product1' AS y, 1 as k_count;
+        """)
+
+
+@mark.parametrize(
+    "query_file, aggregate_values, aggregate_sample, duckdb_file",
+    [
+        (
+            "SQL_Constructor/aggregation_constructor/tests/queries/sum_join_query_1.rq",
+            [AGGREGATION_VAL],
+            AGGREGATION_SAMPLE,
+            "database/aggregation.db",
+        ),
+    ],
+)
+def test_sum_join_output(
+    query_file: str,
+    aggregate_values: list[CompValue],
+    aggregate_sample: CompValue,
+    duckdb_file: str,
+) -> None:
+    """Tests the sum query for aggregation."""
+    reset_seed()
+
+    duckdb_conn = connect(duckdb_file)
+
+    part = get_query_object(readQueryFile(query_file)).algebra
+
+    sum_leaves = all_type_leaves(part, "AggregateJoin")
+
+    # with open(expected_output, "r", encoding="utf-8") as f:
+    #     expected_output_str = f.read()
+
+    for sum_leaf in sum_leaves:
+        output = sum_join_query(
+            aggregate_values,
+            aggregate_sample,
+            sum_leaf,
+        )
+
+        bgp_name = get_table_name(sum_leaf.p.p)
+
+        # Create bgp to pull from
+        _build_bgps(bgp_name, duckdb_conn)
+
+        # Create table
+        create_table_query = (
+            f"CREATE OR REPLACE TABLE {get_table_name(part)} AS " + output
+        )
+        duckdb_conn.execute(create_table_query)
+
+        print("Output:\n", output)
+        # print("Expected Output:\n", expected_output_str)
+
+        assert duckdb_conn.execute(
+            f"SELECT * FROM {get_table_name(part)}"
+        ).fetchall() == [
+            ("Product1", 4, 1),
+        ]
